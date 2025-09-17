077016770758aacba6e91de019ea2a018e22b51e
MahmoodElbadri <mahmoud.elbadry357@gmail.com>
2025-09-17 10:30:39 +0300

    fixing the order of middlewares

--- Restaurant.Api/Program.cs
+++ Restaurant.Api/Program.cs
@@ -6,12 +6,34 @@ using Serilog;
 using Serilog.Events;
 using Restaurant.Api.Middlewares;
 using Restaurant.Domain.Entities;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(tmp =>
+{
+    tmp.AddSecurityDefinition("BearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+    });
+    tmp.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "BearerAuth"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
 builder.Host.UseSerilog((ctx, configureLogger) =>
@@ -29,6 +51,24 @@ app.UseMiddleware<ErrorHandlingMiddleware>(); //we put it in the first line so t
 app.UseMiddleware<TimeLoggingMiddleware>(); //we put it in the first line so that it can log the exact time time taken by each request 
 app.UseSerilogRequestLogging();
 
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseHttpsRedirection();
+app.MapGroup("api/identity").MapIdentityApi<User>();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
 try
 {
     using var scope = app.Services.CreateScope();
@@ -43,17 +83,5 @@ catch (Exception ex)
     Console.WriteLine($"Stack trace: {ex.StackTrace}");
     throw;
 }
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.MapIdentityApi<User>();
-app.UseAuthorization();
-app.MapControllers();
 
 app.Run();
